48e5d7e310a8d24f57f5f18ded501c53ab8965bc
D1elit <dropdox12@gmail.com>
2026-09-14 20:45:13 +0300

    initial

---
+++ supabase/functions/hello/index.ts
@@ -0,0 +1,17 @@
+﻿import "@supabase/functions-js/edge-runtime.d.ts";
+
+export default {
+  fetch(req: Request): Response {
+    if (req.method !== "GET") {
+      return new Response("Method Not Allowed", {
+        status: 405,
+        headers: { Allow: "GET" },
+      });
+    }
+
+    return Response.json({
+      message: "hello, it-incubator",
+      studentId: 5527,
+    });
+  },
+};
